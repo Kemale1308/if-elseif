528a66e4a01613ef4d2b9851f36beb73c462752b
Kemale1308 <esgerovak@gmail.com>
2024-08-26 14:03:31 +0400

    if -else

---
+++ app.js
@@ -0,0 +1,11 @@
+ let anyAge = Number(prompt("Please enter your age"))
+
+ if(anyAge>=18){
+  console.log("You are allowed, come on!")
+ }
+ else if( anyAge<18){
+  console.log("permission was not given")
+ }
+ else{
+  console.log("please enter your AGE correctly")
+ }
